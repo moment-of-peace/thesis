a396f354be51f194e5958aea53b4241308c86943
Yi Liu (刘奕） <yi.liu1183@gmail.com>
2017-12-02 16:57:26 +1000

    modify evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,33 @@
 import sys
 import getopt
+import os
 from sklearn.metrics import f1_score
+
+def append_files(path):
+    flist = os.listdir(path)
+    newFile = 'joint_file.txt'
+    string = ''
+    with open(newFile, 'wt') as tar:
+        for f in flist:
+            with open(os.path.join(path, f), 'rt') as src:
+                for line in src:
+                    if line[:5] != '19 19':
+                        string += line
+            tar.write(string)
+            string = ''
+    return newFile
+
+def merge_entities(fileName):
+    string = ''
+    with open(fileName, 'rt') as src:
+        for line in src:
+            content = line.strip('\n').split(' ')
+            string = '%s%d %d\n'%(string, int(int(content[0])/2), int(int(content[1])/2))
+
+    newFile = 'merged_file.txt'
+    with open(newFile, 'wt') as tar:
+        tar.write(string)
+    return newFile
 
 def f1_eval(fileName, labels):
     tru = []
@@ -17,6 +44,8 @@ def f1_eval(fileName, labels):
 
 def main():
     fileName = '__out__/result_timedis_bi_20e.txt'
+    if len(sys.argv) > 1:
+        fileName = sys.argv[1]
     labels = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 
     options,args = getopt.getopt(sys.argv[1:],"f:d:b:l:m:e:")
